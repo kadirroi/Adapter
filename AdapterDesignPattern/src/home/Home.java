98cd68f588a2fff7a11c18cbd154c19483202822
Kadir <kadirroi@gmail.com>
2015-10-25 23:30:06 +0200

    ad

--- AdapterDesignPattern/src/home/Home.java
+++ AdapterDesignPattern/src/home/Home.java
@@ -18,7 +18,7 @@ public class Home {
 		
 
 	     alaHesapla= new  AdapterDikdortgen();
-		System.out.println("Adaptör Alaný:"+alaHesapla.alanHesapla(20, 5));
+		System.out.println("Adaptör Alaný:"+alaHesapla.alanHesapla(20,
 		
 	}
 
